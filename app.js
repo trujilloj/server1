eab0c32bc74526f15b64b61e1d2401f9859aaa99
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-06-06 14:45:00 -0600

    fix path

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const cohorts = require('./data/cohorts.js')
 
 app.get('/', (req, res) => res.json({data: cohorts}))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen((process.env.PORT || 5000), () => console.log('Example app listening on port 3000!'))
