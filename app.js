d66acf42d983b27c523fb38acd999b24aa505b3d
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-06-06 14:33:36 -0600

    alter get

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const cohorts = require('./data/cohorts.js')
 
-app.get('/', (req, res) => res.send({data: cohorts}))
+app.get('/', (req, res) => res.json({data: cohorts}))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
