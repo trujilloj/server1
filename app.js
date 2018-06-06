eac44d5aed0c560da4de83de8028b24aa0ce3a48
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-06-06 14:19:27 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const cohorts = require('./data/cohorts.js')
+
+app.get('/', (req, res) => res.json({data: cohorts}))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
